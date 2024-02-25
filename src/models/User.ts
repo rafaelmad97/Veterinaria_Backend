213701e6ae8d52e34a917b8ba0329c1fa68670ef
Rafael Madrid <rafaelmad16@outlook.com>
2024-02-25 02:59:11 -0500

    [HU5] Users
    -Se Implementó la api para gestionar el registro de usuarios

---
+++ src/models/User.ts
@@ -0,0 +1,9 @@
+interface User {
+  Id: Number;
+  username: String;
+  email: String;
+  password: String;
+  createAt: Date;
+}
+
+export default User;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,6 +6,13 @@ import {
   updateMedico,
   deleteMedico,
 } from "./MedicosRouter";
+import {
+  createUser,
+  deleteUser,
+  listUsers,
+  listUsersbyId,
+  updateUser,
+} from "./UsersRouter";
 
 const router = Router();
 
@@ -14,5 +21,12 @@ router.get("/medicos/list/:id", listMedicosbyId);
 router.post("/medicos/create", createMedico);
 router.post("/medicos/update/:id", updateMedico);
 router.post("/medicos/delete/:id", deleteMedico);
+
+router.get("/users/list", listUsers);
+router.get("/users/list/:id", listUsersbyId);
+router.post("/users/create", createUser);
+router.post("/users/update/:id", updateUser);
+router.post("/users/delete/:id", deleteUser);
+// Api Usuarios
 
 export default router;
