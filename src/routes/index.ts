ed75f0d445bb633b2c3603084fc9438c619e8357
Rafael Madrid <rafaelmad16@outlook.com>
2024-02-25 02:12:33 -0500

    [HU6] Citas
    -Se ha implementado la api para la gestión de las citas
    - Se implementó el orden de las citas de forma ascendente.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,6 +6,12 @@ import {
   updateMedico,
   deleteMedico,
 } from "./MedicosRouter";
+import {
+  createCitas,
+  deleteCitas,
+  listCitas,
+  updateCitas,
+} from "./CitasRoutes";
 
 const router = Router();
 
@@ -14,5 +20,12 @@ router.get("/medicos/list/:id", listMedicosbyId);
 router.post("/medicos/create", createMedico);
 router.post("/medicos/update/:id", updateMedico);
 router.post("/medicos/delete/:id", deleteMedico);
+/// Api Medicos
+router.get("/citas/list", listCitas);
+router.get("/citas/list/:id", listMedicosbyId);
+router.post("/citas/create", createCitas);
+router.post("/citas/update/:id", updateCitas);
+router.post("/citas/delete/:id", deleteCitas);
+/// api Citas
 
 export default router;
