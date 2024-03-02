2ca13a5a6213f0a46afc2ebe95aeacd4c8bc5066
Rafael Madrid <rafaelmad16@outlook.com>
2024-03-01 21:49:58 -0500

    [HU-9] Personas
    - Se implementó la entidad de la tabla Personas para el nuevo modelo

---
+++ src/models/Persona.ts
@@ -0,0 +1,11 @@
+interface Persona {
+  id: Number;
+  Nombres: String;
+  Apellidos: String;
+  birthdate: Date;
+  email: String;
+  telefono: String;
+  recibeNotificaciones: boolean;
+}
+
+export default Persona;
